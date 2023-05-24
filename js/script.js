3d032f20f04843c3cccbde69cdcc1194de4fcfb7
Macaroo <kmacaro67j2@gmail.com>
2023-05-24 22:36:10 +0900

    ハンバーガー,フッター修正

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-$(function () {});
+//ハンバーガーメニュー
+$(function () {
+    $(".js-hamburger, .js-drawer").click(function () {
+    //toggleClassでclassをつけたり外したりする
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+});
+
+});
